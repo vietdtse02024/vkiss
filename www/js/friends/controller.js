cf58ce6532de9e9cab368ec4ca77bc87f7e55bd5
Anh Viet <vietdtse02024@gmail.com>
2015-07-30 17:24:18 +0700

    commit 30.07.2015

--- www/js/friends/controller.js
+++ www/js/friends/controller.js
@@ -1,7 +1,14 @@
 ﻿angular.module('fyviapp')
-.controller('FriendsCtrl', function ($scope, Chats) {
+.controller('FriendsCtrl', function ($scope, Chats, $http) {
     $scope.chats = Chats.all();
+    
     $scope.remove = function (chat) {
         Chats.remove(chat);
     }
+
+    $scope.getListFriends = function () {
+        $http.get('http://localhost:8082/fyvi-ws/fyvi/account/get-list-friends').success(function (response) {
+            $scope.model = response;
+        });
+    };
 });
